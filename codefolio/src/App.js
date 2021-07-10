1d707d5ee3c438dacc2f4cf4ffa978033ab97c0a
AimeeJay <ajesso@post.com>
2021-07-10 00:35:18 -0400

    reverted to last working commit the best i could

--- codefolio/src/App.js
+++ codefolio/src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import './App.css';
 import AboutMe from './components/AboutMe';
-import Contact from './components/Contact/Contact';
+import Contact from './components/Contact';
 import Nav from './components/Nav';
-import Portfolio from './components/Portfolio';
+// import Portfolio from './components/Portfolio';
 import Resume from './components/Resume';
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -18,8 +18,8 @@ function App() {
             <Route exact path="/" component={Home} />
             <Route exact path="/aboutme" component={AboutMe} />
             <Route exact path="/contact" component={Contact} />
-            <Route path="/portfolio" component={Portfolio} />
-            {/* <Route path="/resume" component={Resume}/> */}
+            {/* <Route exact path="/portfolio" component={Portfolio} /> */}
+            {/* <Route exact path="/resume" component={Resume}/> */}
           </Switch>
         </Router>
       </main>
--- codefolio/src/components/CodeCards.js
+++ codefolio/src/components/CodeCards.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
+
 //Materialize import
 
-//create cards to map and display
-function myProjects(props) {
+create cards to map and display
+function myProjects = (props) => {
     <div class="col s4">
         <div class="card">
             <div class="card-image">
@@ -20,73 +21,73 @@ function myProjects(props) {
     </div>
 }
 
-function Codecards() {
-    const [carddata] = [
-        {
-          id: 1,
-          name: "Run Buddy",
-          image: "react-porfolio/codefolio/src/assets/img/runbuddy.png",
-          github: "https://github.com/MissAimeeJay/Run-Buddy",
-          livelink: "https://missaimeejay.github.io/Run-Buddy/"
-      },
-      {
-          id: 2,
-          name: "Horoscopes",
-          image: "react-porfolio/codefolio/src/assets/img/horoscopes.png",
-          github: "https://github.com/MissAimeeJay/myFirstHoroscopeApi",
-          livelink: "https://missaimeejay.github.io/myFirstHoroscopeApi/"
-      },
-      {
-          id: 3,
-          name: "LocalShop",
-          image: "react-porfolio/codefolio/src/assets/img/localshop.png",
-          github: "https://github.com/chaitalizn/localshop",
-          livelink: "https://shop-local-with-localshop.herokuapp.com"
-      },
-      {
-          id: 4,
-          name: "Drinkspo",
-          image: "react-porfolio/codefolio/src/assets/img/drinkspo.png",
-          github: "https://github.com/PrimalOrB/find_your_drink",
-          livelink: "https://primalorb.github.io/find_your_drink/"
-      },
-      {
-          id: 5,
-          name: "Weather Reporter",
-          image: "react-porfolio/codefolio/src/assets/img/weatherreporter.png",
-          github: "https://github.com/MissAimeeJay/WeatherDashboardAPI",
-          livelink: "https://missaimeejay.github.io/WeatherDashboardAPI/"
-      },
-      {
-          id: 6,
-          name: "Express Note Taker",
-          image: "react-porfolio/codefolio/src/assets/img/notetaker.png",
-          github: "https://github.com/MissAimeeJay/Express-Note-Taker",
-          livelink: "https://express-note-takr.herokuapp.com/"
-      },
-      {
-          id: 7,
-          name: "Work Day Scheduler",
-          image: "rreact-porfolio/codefolio/src/assets/img/workday.png",
-          github: "https://github.com/MissAimeeJay/WorkDaySchedulerChallenge",
-          livelink: "https://missaimeejay.github.io/WorkDaySchedulerChallenge/"
-      },
-      {
-          id: 8,
-          name: "Password Generator",
-          image: "react-porfolio/codefolio/src/assets/img/passgen.png",
-          github: "https://github.com/MissAimeeJay/PasswordGeneratorChallenge",
-          livelink: "https://missaimeejay.github.io/PasswordGeneratorChallenge/"
-      },
-      {
-          id: 9,
-          name: "Horiseon Social Services Code Refactor",
-          image: "react-porfolio/codefolio/src/assets/img/horiseon.png",
-          github: "https://github.com/MissAimeeJay/HoriseonSocialSolutionServices",
-          livelink: "https://missaimeejay.github.io/HoriseonSocialSolutionServices/"
-      }
-  ];
-}
+// function Codecards() {
+//     const [carddata] = [
+//         {
+//           id: 1,
+//           name: "Run Buddy",
+//           image: "react-porfolio/codefolio/src/assets/img/runbuddy.png",
+//           github: "https://github.com/MissAimeeJay/Run-Buddy",
+//           livelink: "https://missaimeejay.github.io/Run-Buddy/"
+//       },
+//       {
+//           id: 2,
+//           name: "Horoscopes",
+//           image: "react-porfolio/codefolio/src/assets/img/horoscopes.png",
+//           github: "https://github.com/MissAimeeJay/myFirstHoroscopeApi",
+//           livelink: "https://missaimeejay.github.io/myFirstHoroscopeApi/"
+//       },
+//       {
+//           id: 3,
+//           name: "LocalShop",
+//           image: "react-porfolio/codefolio/src/assets/img/localshop.png",
+//           github: "https://github.com/chaitalizn/localshop",
+//           livelink: "https://shop-local-with-localshop.herokuapp.com"
+//       },
+//       {
+//           id: 4,
+//           name: "Drinkspo",
+//           image: "react-porfolio/codefolio/src/assets/img/drinkspo.png",
+//           github: "https://github.com/PrimalOrB/find_your_drink",
+//           livelink: "https://primalorb.github.io/find_your_drink/"
+//       },
+//       {
+//           id: 5,
+//           name: "Weather Reporter",
+//           image: "react-porfolio/codefolio/src/assets/img/weatherreporter.png",
+//           github: "https://github.com/MissAimeeJay/WeatherDashboardAPI",
+//           livelink: "https://missaimeejay.github.io/WeatherDashboardAPI/"
+//       },
+//       {
+//           id: 6,
+//           name: "Express Note Taker",
+//           image: "react-porfolio/codefolio/src/assets/img/notetaker.png",
+//           github: "https://github.com/MissAimeeJay/Express-Note-Taker",
+//           livelink: "https://express-note-takr.herokuapp.com/"
+//       },
+//       {
+//           id: 7,
+//           name: "Work Day Scheduler",
+//           image: "rreact-porfolio/codefolio/src/assets/img/workday.png",
+//           github: "https://github.com/MissAimeeJay/WorkDaySchedulerChallenge",
+//           livelink: "https://missaimeejay.github.io/WorkDaySchedulerChallenge/"
+//       },
+//       {
+//           id: 8,
+//           name: "Password Generator",
+//           image: "react-porfolio/codefolio/src/assets/img/passgen.png",
+//           github: "https://github.com/MissAimeeJay/PasswordGeneratorChallenge",
+//           livelink: "https://missaimeejay.github.io/PasswordGeneratorChallenge/"
+//       },
+//       {
+//           id: 9,
+//           name: "Horiseon Social Services Code Refactor",
+//           image: "react-porfolio/codefolio/src/assets/img/horiseon.png",
+//           github: "https://github.com/MissAimeeJay/HoriseonSocialSolutionServices",
+//           livelink: "https://missaimeejay.github.io/HoriseonSocialSolutionServices/"
+//       }
+//   ];
+// }
 
 
 export default myProjects;
