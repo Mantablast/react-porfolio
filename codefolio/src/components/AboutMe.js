eada3ddabb2819bbf37d54f8ad8d211967f0bfa5
AimeeJay <ajesso@post.com>
2021-07-10 12:16:17 -0400

    about me fixes made

--- codefolio/src/components/AboutMe.js
+++ codefolio/src/components/AboutMe.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function AboutMe() {
   return (
-    <section className="my-5">
-        <span className="white-text text-darken-2">About me Information : Show up, do the work, always learn, eat the snacks and do better than yesterday.  Make way for efficiencies, problem solves, and money savings.</span>
+    <section className="my-5 back-box">
+        <span className="white-text text-darken-2">About me Information : Show up, do the work, always learn, eat the snacks and do better than yesterday.  Make way for efficiencies, problem solves, and money savings.  I have been studying with the University of Toronto for full stack web development and have been helping Carfax Inc process vehicle accident data in all american states.</span>
     </section>
   );
 }
